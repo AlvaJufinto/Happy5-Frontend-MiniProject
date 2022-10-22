d28fcbd25248c857078610b1cc45292e4413a689
unknown <yeahbruh199@gmail.com>
2022-10-22 14:09:04 +0700

    ADD: data.ts to store dummy data

--- src/components/templates/RoadmapContainer.tsx
+++ src/components/templates/RoadmapContainer.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react';
 import styled from 'styled-components';
 
 import InfoContainer from '../organisms/InfoContainer';
+import { productRoadmap } from '../../data';
 
 const StyledRoadmapContainer = styled.div`
   margin: 24px 0;
@@ -12,47 +13,6 @@ const StyledRoadmapContainer = styled.div`
 `
 
 const RoadmapContainer: FC = () => {
-  const productRoadmap = [
-    {
-      year: 'Q1 2019',
-      period: 'January - March',
-      tasks: [
-        {
-          title: 'Re-designed the zero-g doggie bags. No more spills!',
-          percentage: '64%'
-        },
-        {
-          title: 'Travel & Relocation Support',
-          percentage: '12%'
-        }
-      ]
-    },
-    {
-      year: 'Q2 2019',
-      period: 'April - June',
-    },
-    {
-      year: 'Q3 2019',
-      period: 'July - September',
-      tasks: [
-        {
-          title: 'Bundle interplanetary analytics for improved transmission',
-          percentage: '90%'
-        }
-      ]
-    },
-    {
-      year: 'Q4 2019',
-      period: 'October - December',
-      tasks: [
-        {
-          title: 'Data Migration: Performance & Culture End Game',
-          percentage: '63%'
-        }
-      ]
-    },
-  ]
-
   return (
     <StyledRoadmapContainer>
         {productRoadmap.map(product => (
---
+++ src/data.ts
@@ -0,0 +1,40 @@
+export const productRoadmap = [
+    {
+      year: 'Q1 2019',
+      period: 'January - March',
+      tasks: [
+        {
+          title: 'Re-designed the zero-g doggie bags. No more spills!',
+          percentage: '64%'
+        },
+        {
+          title: 'Travel & Relocation Support',
+          percentage: '12%'
+        }
+      ]
+    },
+    {
+      year: 'Q2 2019',
+      period: 'April - June',
+    },
+    {
+      year: 'Q3 2019',
+      period: 'July - September',
+      tasks: [
+        {
+          title: 'Bundle interplanetary analytics for improved transmission',
+          percentage: '90%'
+        }
+      ]
+    },
+    {
+      year: 'Q4 2019',
+      period: 'October - December',
+      tasks: [
+        {
+          title: 'Data Migration: Performance & Culture End Game',
+          percentage: '63%'
+        }
+      ]
+    },
+  ]
